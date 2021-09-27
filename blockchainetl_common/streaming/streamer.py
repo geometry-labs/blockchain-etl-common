2aed28753efe6a6871576bd88a5b7c94aa2b05ba
Richard Mah <dick.mah@gmail.com>
2021-09-27 08:32:31 -0700

    Add basic streaming metrics

--- blockchainetl_common/streaming/streamer.py
+++ blockchainetl_common/streaming/streamer.py
@@ -28,19 +28,28 @@ import time
 from blockchainetl_common.streaming.streamer_adapter_stub import StreamerAdapterStub
 from blockchainetl_common.file_utils import smart_open
 
+from prometheus_client import Gauge
+
+
+START_BLOCK = Gauge("start_block", "Start block height")
+END_BLOCK = Gauge("end_block", "End block height")
+TARGET_BLOCK = Gauge("target_block", "Target block height")
+LAST_SYNCED_BLOCK = Gauge("last_synced_block", "Last synced block height")
+
 
 class Streamer:
     def __init__(
-            self,
-            blockchain_streamer_adapter=StreamerAdapterStub(),
-            last_synced_block_file='last_synced_block.txt',
-            lag=0,
-            start_block=None,
-            end_block=None,
-            period_seconds=10,
-            block_batch_size=10,
-            retry_errors=True,
-            pid_file=None):
+        self,
+        blockchain_streamer_adapter=StreamerAdapterStub(),
+        last_synced_block_file="last_synced_block.txt",
+        lag=0,
+        start_block=None,
+        end_block=None,
+        period_seconds=10,
+        block_batch_size=10,
+        retry_errors=True,
+        pid_file=None,
+    ):
         self.blockchain_streamer_adapter = blockchain_streamer_adapter
         self.last_synced_block_file = last_synced_block_file
         self.lag = lag
@@ -51,61 +60,87 @@ class Streamer:
         self.retry_errors = retry_errors
         self.pid_file = pid_file
 
-        if self.start_block is not None or not os.path.isfile(self.last_synced_block_file):
-            init_last_synced_block_file((self.start_block or 0) - 1, self.last_synced_block_file)
+        if self.start_block is not None or not os.path.isfile(
+            self.last_synced_block_file
+        ):
+            init_last_synced_block_file(
+                (self.start_block or 0) - 1, self.last_synced_block_file
+            )
 
         self.last_synced_block = read_last_synced_block(self.last_synced_block_file)
+
+        START_BLOCK.set(start_block)
+        END_BLOCK.set(end_block)
 
     def stream(self):
         try:
             if self.pid_file is not None:
-                logging.info('Creating pid file {}'.format(self.pid_file))
+                logging.info("Creating pid file {}".format(self.pid_file))
                 write_to_file(self.pid_file, str(os.getpid()))
             self.blockchain_streamer_adapter.open()
             self._do_stream()
         finally:
             self.blockchain_streamer_adapter.close()
             if self.pid_file is not None:
-                logging.info('Deleting pid file {}'.format(self.pid_file))
+                logging.info("Deleting pid file {}".format(self.pid_file))
                 delete_file(self.pid_file)
 
     def _do_stream(self):
-        while True and (self.end_block is None or self.last_synced_block < self.end_block):
+        while True and (
+            self.end_block is None or self.last_synced_block < self.end_block
+        ):
             synced_blocks = 0
 
             try:
                 synced_blocks = self._sync_cycle()
             except Exception as e:
                 # https://stackoverflow.com/a/4992124/1580227
-                logging.exception('An exception occurred while syncing block data.')
+                logging.exception("An exception occurred while syncing block data.")
                 if not self.retry_errors:
                     raise e
 
             if synced_blocks <= 0:
-                logging.info('Nothing to sync. Sleeping for {} seconds...'.format(self.period_seconds))
+                logging.info(
+                    "Nothing to sync. Sleeping for {} seconds...".format(
+                        self.period_seconds
+                    )
+                )
                 time.sleep(self.period_seconds)
 
     def _sync_cycle(self):
         current_block = self.blockchain_streamer_adapter.get_current_block_number()
 
-        target_block = self._calculate_target_block(current_block, self.last_synced_block)
+        target_block = self._calculate_target_block(
+            current_block, self.last_synced_block
+        )
+        TARGET_BLOCK.set(target_block)
         blocks_to_sync = max(target_block - self.last_synced_block, 0)
 
-        logging.info('Current block {}, target block {}, last synced block {}, blocks to sync {}'.format(
-            current_block, target_block, self.last_synced_block, blocks_to_sync))
+        logging.info(
+            "Current block {}, target block {}, last synced block {}, blocks to sync {}".format(
+                current_block, target_block, self.last_synced_block, blocks_to_sync
+            )
+        )
 
         if blocks_to_sync != 0:
-            self.blockchain_streamer_adapter.export_all(self.last_synced_block + 1, target_block)
-            logging.info('Writing last synced block {}'.format(target_block))
+            self.blockchain_streamer_adapter.export_all(
+                self.last_synced_block + 1, target_block
+            )
+            logging.info("Writing last synced block {}".format(target_block))
             write_last_synced_block(self.last_synced_block_file, target_block)
             self.last_synced_block = target_block
+            LAST_SYNCED_BLOCK.set(target_block)
 
         return blocks_to_sync
 
     def _calculate_target_block(self, current_block, last_synced_block):
         target_block = current_block - self.lag
         target_block = min(target_block, last_synced_block + self.block_batch_size)
-        target_block = min(target_block, self.end_block) if self.end_block is not None else target_block
+        target_block = (
+            min(target_block, self.end_block)
+            if self.end_block is not None
+            else target_block
+        )
         return target_block
 
 
@@ -117,23 +152,25 @@ def delete_file(file):
 
 
 def write_last_synced_block(file, last_synced_block):
-    write_to_file(file, str(last_synced_block) + '\n')
+    write_to_file(file, str(last_synced_block) + "\n")
 
 
 def init_last_synced_block_file(start_block, last_synced_block_file):
     if os.path.isfile(last_synced_block_file):
         raise ValueError(
-            '{} should not exist if --start-block option is specified. '
-            'Either remove the {} file or the --start-block option.'
-                .format(last_synced_block_file, last_synced_block_file))
+            "{} should not exist if --start-block option is specified. "
+            "Either remove the {} file or the --start-block option.".format(
+                last_synced_block_file, last_synced_block_file
+            )
+        )
     write_last_synced_block(last_synced_block_file, start_block)
 
 
 def read_last_synced_block(file):
-    with smart_open(file, 'r') as last_synced_block_file:
+    with smart_open(file, "r") as last_synced_block_file:
         return int(last_synced_block_file.read())
 
 
 def write_to_file(file, content):
-    with smart_open(file, 'w') as file_handle:
+    with smart_open(file, "w") as file_handle:
         file_handle.write(content)
--- setup.py
+++ setup.py
@@ -7,41 +7,42 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-long_description = read('README.md') if os.path.isfile("README.md") else ""
+long_description = read("README.md") if os.path.isfile("README.md") else ""
 
 setup(
-    name='blockchain-etl-common',
-    version='1.6.1',
-    author='Evgeny Medvedev',
-    author_email='evge.medvedev@gmail.com',
-    description='Common utils for Blockchain ETL',
+    name="blockchain-etl-common-instrumented",
+    version="1.6.2-alpha.1",
+    author="Evgeny Medvedev",
+    author_email="evge.medvedev@gmail.com",
+    maintainer="Richard Mah",
+    maintainer_email="richard@geometrylabs.io",
+    description="Common utils for Blockchain ETL",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/blockchain-etl/blockchain-etl-common',
-    packages=find_packages(exclude=['schemas', 'tests']),
+    long_description_content_type="text/markdown",
+    url="https://github.com/blockchain-etl/blockchain-etl-common",
+    packages=find_packages(exclude=["schemas", "tests"]),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='blockchain ethereum bitcoin etl',
-    python_requires='>=3.5.3,<4',
-    install_requires=[
-    ],
+    keywords="blockchain ethereum bitcoin etl",
+    python_requires=">=3.5.3,<4",
+    install_requires=[],
     extras_require={
-        'streaming': [
-            'timeout-decorator==0.4.1',
-            'google-cloud-pubsub==2.1.0',
-            'google-cloud-storage==1.33.0',
+        "streaming": [
+            "timeout-decorator==0.4.1",
+            "google-cloud-pubsub==2.1.0",
+            "google-cloud-storage==1.33.0",
+            "prometheus-client ~= 0.11.0",
         ]
     },
     project_urls={
-        'Bug Reports': 'https://github.com/blockchain-etl/blockchain-etl-common/issues',
-        'Chat': 'https://gitter.im/ethereum-etl/Lobby',
-        'Source': 'https://github.com/blockchain-etl/blockchain-etl-common',
+        "Bug Reports": "https://github.com/geometry-labs/blockchain-etl-common/issues",
+        "Source": "https://github.com/geometry-labs/blockchain-etl-common",
     },
 )
